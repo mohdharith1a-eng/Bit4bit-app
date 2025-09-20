cfc186c0ae6e31507cf0846781065969faad8869
mohdharith1a-eng <mohdharith1a@gmail.com>
2025-09-20 23:41:50 +0800

    Update Apps5.py

--- Apps5.py
+++ Apps5.py
@@ -106,7 +106,7 @@ def add_bg_from_local(image_file):
     st.markdown(css, unsafe_allow_html=True)
 
 # ================== PATHS & DATA LOADING ==================
-gdp_path = r"C:\Users\Acer\OneDrive\Desktop\Apps3\datasets\gdp_state_.csv"
+gdp_path = r"https://github.com/mohdharith1a-eng/Bit4bit-app/blob/main/datasets/gdp_state_.csv"
 unemp_path = r"C:\Users\Acer\OneDrive\Desktop\Apps3\datasets\unemployed.csv"
 pop_path = r"C:\Users\Acer\OneDrive\Desktop\Apps3\datasets\population_state.csv"
 wellbeing_path = r"C:\Users\Acer\OneDrive\Desktop\Apps3\datasets\economic_wellbeing.csv"
@@ -600,4 +600,5 @@ with tab2:
             st.write("**Chartbot Answer:**")
             st.write(jawapan)
         else:
+
             st.warning("Please enter a question first.")
